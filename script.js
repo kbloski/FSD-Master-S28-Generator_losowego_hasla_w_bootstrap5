0237d3e5b6a72fa4ee1fd807d4bbcbe017938268
Kamil Błoński <kblonski02@gmail.com>
2024-08-08 11:01:39 +0200

    Complete project

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ class PasswordGenerator {
         this.length = document.querySelector('#length');
         this.lengthRange = document.querySelector('#lengthRange');
 
-        this.lowercase = document.querySelector('#lowercase');
-        this.uppercase = document.querySelector('#uppercase');
+        this.lowercaseCheckbox = document.querySelector('#lowercase');
+        this.uppercaseCheckbox = document.querySelector('#uppercase');
 
         this.numbersCheckbox = document.querySelector('#numbers');
         this.symbolsCheckbox = document.querySelector('#symbols');
@@ -30,16 +30,18 @@ class PasswordGenerator {
     }
 
     updateOptions(){
+        const optionsMethods = [];
+
+        if (this.uppercaseCheckbox.checked) optionsMethods.push(this.getRandomUppercase);
+        if (this.lowercaseCheckbox.checked) optionsMethods.push(this.getRandomLowercase)
+        if (this.numbersCheckbox.checked) optionsMethods.push(this.getRandomNumber)
+        if (this.symbolsCheckbox.checked) optionsMethods.push(this.getRandomSymbol)
+
+        this.optionsMethods = optionsMethods;
 
     }
 
-    generatePassword = () => {
-
-    }
-
-    copyToClipboard = () => {
-
-    }
+    
 
     rangeLengthChange = () => {
         this.length.value = this.lengthRange.value
@@ -48,6 +50,48 @@ class PasswordGenerator {
 
     lengthChange = () => {
         this.lengthRange.value = this.length.value
+    }
+
+    getRandomUppercase = () => { // 65 - 90
+        return String.fromCharCode( 65 + Math.floor( Math.random() * 26 ) );
+    }
+
+    getRandomLowercase = () => {    // asci 97 - 120
+        return String.fromCharCode( 97 + Math.floor( Math.random() * 24 ) );
+    }
+
+    getRandomNumber = () => {
+        return Math.floor( Math.random() * 10 )
+    }
+
+    getRandomSymbol = () => {
+        const symbols = `!@#$%^&*()_+={}[<>`;
+        return symbols[ Math.floor(Math.random() * symbols.length )]
+    }
+
+    generatePassword = () => {
+       if (!this.length.value) return;
+       if (this.optionsMethods.length === 0) return;
+
+       const arrIndexes = Array.from(Array(parseInt(this.length.value)).keys());
+
+       const password = arrIndexes.map(i => {
+        const method = this.getRandomMethod();
+        return method();
+       }).join('');
+
+       this.resultPassword.value = password;
+       
+    }
+
+    getRandomMethod = () => {
+        return this.optionsMethods[ Math.floor( Math.random() * this.optionsMethods.length)];
+    }
+
+    copyToClipboard = () => {
+        const v = this.resultPassword.value;
+        const cb = navigator.clipboard;
+        cb.writeText(v).then( ()=>{console.log('Password in clipboard!')})
     }
 
 
